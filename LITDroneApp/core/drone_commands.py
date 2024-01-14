4ab2f8a44f56adc990e597d0c801eb3cfb9615f7
Prikop Kirill <90692541+DeathGun2303@users.noreply.github.com>
2024-01-14 16:55:32 +0100

    sjkgbae
    ndasnkjg

--- LITDroneApp/core/drone_commands.py
+++ LITDroneApp/core/drone_commands.py
@@ -3,8 +3,9 @@ import cv2
 import time
 from threading import Thread
 
+### Basic Tests Commands 
+#class CommandsStrategy:
 tello = Tello()
-# Takeoff in das Switch-case Dingsbums
 
 def connect_to_drone():
     print("Connect to Tello Drone")
@@ -12,51 +13,54 @@ def connect_to_drone():
 
 def take_off_land():
     print("Sleep for 5 seconds")
+    tello.takeoff()
     time.sleep(5)
 
-def move_up_down():
+def move_up():
     print("Move Up")
     tello.move_up(40)
 
+def move_down():
     print("Move Down")
     tello.move_down(40)
 
-def move_left_right():
+def move_left():
     print("Move Left")
     tello.move_left(40)
 
+def move_right():
     print("Move Right")
     tello.move_right(40)
 
-def move_forward_backwards():
+def move_forward():
     print("Move Forward")
     tello.move_forward(40)
 
+def move_backwards():
     print("Move Backwards")
     tello.move_back(40)
 
-def rotate_cw_ccw():
+def rotate_cw():
     print("Rotate Clockwise")
     tello.rotate_clockwise(90)
 
+def rotate_ccw():
     print("Rotate Counter Clockwise")
     tello.rotate_counter_clockwise(90)
 
-def flip_left_right():
+def flip_left():
     print("Flip left")
     tello.flip_left()
 
-    time.sleep(2)
-
+def flipt_right():
     print("Flip Right")
     tello.flip_right()
 
-def flip_forward_backwards():
+def flip_forward():
     print("Flip forward")
     tello.flip_forward
 
-    time.sleep(2)
-
+def flip_backwards():
     print("Flip backwards")
     tello.flip_back
 
@@ -421,3 +425,58 @@ def object_detection():
 
     keepRecording = False
     recorder.join()
+    
+
+input = ""
+
+match input:
+    case "connect":
+        connect_to_drone()
+    case "takeoff":
+        tello.takeoff()
+    case "land":
+        tello.land()
+    case "move forward":
+        move_forward()
+    case "move backwards":
+        move_backwards()
+    case "move left":
+        move_left()
+    case "move right":
+        move_right()
+    case "move up":
+        move_up()
+    case "move down":
+        move_down()
+    case "rotate clockwise":
+        rotate_cw()
+    case "rotate counter clockwise":
+        rotate_ccw()
+    case "flip left":
+        flip_left()
+    case "flip right":
+        flipt_right()
+    case "flip forward":
+        flip_forward()
+    case "flip backwards":
+        flip_backwards()
+    case "go xyz":
+        go_xyz()
+    case "criss cross":
+        criss_cross()
+    case "send rc control async":
+        send_rc_control_async()
+    case "take picture":
+        take_picture()
+    case "video feed no flying":
+        video_feed_no_flying()
+    case "video feed flying":
+        video_feed_flying()
+    case "video feed flying synchronous":
+        video_feed_flying_synchronous
+    case "object detection":
+        object_detection()
+    case "fly curve":
+        # needs imput params
+        tello.curve_xyz_speed()
+        beans
