c3fbce8d2aed3fbb9cc52f0ffeb2df5a540c86e7
Prikop Kirill <90692541+DeathGun2303@users.noreply.github.com>
2024-01-14 19:11:11 +0100

    class drone

--- LITDroneApp/core/drone_controller.py
+++ LITDroneApp/core/drone_controller.py
@@ -1,17 +1,18 @@
 from .drone_commands import *
 
-def connection():
-    try:
-        connect_to_drone()
-    except:
-        print("failed to connect to drone")
+class drone:
+    def connection():
+        try:
+            connect_to_drone()
+        except:
+            print("failed to connect to drone")
 
-def start():
-    take_off_land()
-    print("flight succsessful")
+    def start():
+        take_off_land()
+        print("flight succsessful")
 
-def rotationTest():
-    send_rc_control_async()
-    print("flight succsessful")
+    def rotationTest():
+        send_rc_control_async()
+        print("flight succsessful")
 
     
