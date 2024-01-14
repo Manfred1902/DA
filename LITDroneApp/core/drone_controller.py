19dccd5249ee558fcb5e80167c6a2245dea00d37
Prikop Kirill <90692541+DeathGun2303@users.noreply.github.com>
2024-01-14 17:41:11 +0100

    start takeoff
    beep

--- LITDroneApp/core/drone_controller.py
+++ LITDroneApp/core/drone_controller.py
@@ -3,9 +3,14 @@ from drone_commands import *
 def connection():
     try:
         connect_to_drone()
-
     except:
         print("failed to connect to drone")
+
+def start():
+    take_off_land()
+    print("flight succsessful")
+
+
     
 
     
