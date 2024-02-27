8f9b58f703da450932d9b5a1bec2041fb8daa5d8
Manfred1902 <170159@studierende.htl-donaustadt.at>
2024-02-27 14:43:29 +0100

    app
    DANIEL DU KANNST MICH KREUZWEISE AN MEINEM SEXY THICC TRAINIERTEN ARSCH LECKEN

--- camera stream app/main.py
+++ camera stream app/main.py
@@ -1,34 +1,31 @@
-from kivymd.app import MDApp
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.button import MDRaisedButton
+from djitellopy import tello
+import cv2
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.graphics.texture import Texture
 from kivy.clock import Clock
-import cv2
 
-class MainApp(MDApp):
+class TelloImageApp(App):
+    me = tello.Tello()
+    me.connect()
+    me.streamoff()
+    me.streamon()
     def build(self):
-        layout = MDBoxLayout(orientation = 'vertical')
-        self.image = Image()
-        layout.add_widget(self.image)
-        layout.add_widget(MDRaisedButton(
-            text = 'click here',
-            pos_hint = {'center_x': .5, 'center_y': .5},
-             size_hint =(None, None))
-        )
-
-        self.capture = cv2.VideoCapture(0)
-        Clock.schedule_interval(self.load_video, 1.0/30.0)
+        layout = BoxLayout(orientation='vertical')
+        self.image_widget = Image()
+        layout.add_widget(self.image_widget)
+        Clock.schedule_interval(self.update_image, 1.0 / 30.0)
         return layout
-    
-    def load_video(self, *args):
-        ret, frame = self.capture.read()
-        # Frame initialize
-        self.image_frame = frame
-        buffer = cv2.flip(frame, 0).tostring()
-        texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+
+    def update_image(self, dt):
+        myFrame = self.me.get_frame_read().frame
+        image = cv2.resize(myFrame, (360, 240))
+        buffer = cv2.flip(image, 0).tobytes()
+        texture = Texture.create(size=(image.shape[1], image.shape[0]), colorfmt='bgr')
         texture.blit_buffer(buffer, colorfmt='bgr', bufferfmt='ubyte')
-        self.image.texture = texture
+        self.image_widget.texture = texture
+        
 
 if __name__ == '__main__':
-    MainApp().run()
+    TelloImageApp().run()
