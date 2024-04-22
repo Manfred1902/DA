eb7d2c656e7dbf5bd3580bbb982fab4d38fbe9c1
Manfred1902 <170159@studierende.htl-donaustadt.at>
2024-04-22 09:41:56 +0200

    .

--- camera stream app/main.py
+++ camera stream app/main.py
@@ -1,3 +1,4 @@
+'''
 from djitellopy import tello
 import cv2
 from kivy.app import App
@@ -5,12 +6,21 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.graphics.texture import Texture
 from kivy.clock import Clock
+from ultralytics import YOLO
 
 class TelloImageApp(App):
-    me = tello.Tello()
-    me.connect()
-    me.streamoff()
-    me.streamon()
+    # load yolov8 model
+    model = YOLO('yolov8n.pt')
+
+    #me = tello.Tello()
+    #me.connect()
+    #me.streamoff()
+    #me.streamon()
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)
+    cap.set(4, 480)
+    ret = True
+
     def build(self):
         layout = BoxLayout(orientation='vertical')
         self.image_widget = Image()
@@ -19,13 +29,70 @@ class TelloImageApp(App):
         return layout
 
     def update_image(self, dt):
-        myFrame = self.me.get_frame_read().frame
-        image = cv2.resize(myFrame, (360, 240))
-        buffer = cv2.flip(image, 0).tobytes()
-        texture = Texture.create(size=(image.shape[1], image.shape[0]), colorfmt='rgb')
-        texture.blit_buffer(buffer, colorfmt='rgb', bufferfmt='ubyte')
+        self.ret, frame = self.cap.read()
+        results = self.model.track(frame, persist=True)
+        frame_ = results[0].plot()
+
+        # Flip the frame vertically
+        frame_ = cv2.flip(frame_, 0)
+
+        texture = Texture.create(size=(frame_.shape[1], frame_.shape[0]), colorfmt='bgr')
+        texture.blit_buffer(frame_.tostring(), colorfmt='bgr', bufferfmt='ubyte')
+
         self.image_widget.texture = texture
-        
+
+
+
+
+if __name__ == '__main__':
+    TelloImageApp().run()
+    '''
+
+from djitellopy import Tello
+import cv2
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.graphics.texture import Texture
+from kivy.clock import Clock
+from ultralytics import YOLO
+
+class TelloImageApp(App):
+    # load yolov8 model
+    model = YOLO('yolov8n.pt')
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.tello = Tello()
+
+    def build(self):
+        layout = BoxLayout(orientation='vertical')
+        self.image_widget = Image()
+        layout.add_widget(self.image_widget)
+        Clock.schedule_interval(self.update_image, 1.0 / 30.0)
+        return layout
+
+    def update_image(self, dt):
+        frame = self.tello.get_frame_read().frame
+        results = self.model.track(frame, persist=True)
+        frame_ = results[0].plot()
+
+        # Flip the frame vertically
+        frame_ = cv2.flip(frame_, 0)
+
+        texture = Texture.create(size=(frame_.shape[1], frame_.shape[0]), colorfmt='bgr')
+        texture.blit_buffer(frame_.tostring(), colorfmt='bgr', bufferfmt='ubyte')
+
+        self.image_widget.texture = texture
+
+    def on_start(self):
+        self.tello.connect()
+        self.tello.streamoff()
+        self.tello.streamon()
+
+    def on_stop(self):
+        self.tello.streamoff()
+        self.tello.end()
 
 if __name__ == '__main__':
     TelloImageApp().run()
---
+++ follow-face/FaceTrackingTello.py
@@ -0,0 +1,34 @@
+from utils import *
+import cv2
+
+w, h = 360, 240
+pid = [0.5, 0.5, 0]
+pError = 0
+startCounter = 1 # for no Flight 1  - for flight 0
+
+myDrone = initializeTello()
+
+while True:
+    ## Flight
+    if startCounter == 0:
+        myDrone.takeoff()
+        myDrone.move_up(20) # Input entspricht der Einheit cm
+        # Input: 20 -> 20cm hoch
+        # Input Values: 20 - 500
+        startCounter = 1
+
+    ## Step 1
+    img = telloGetFrame(myDrone, w, h,)
+
+    ## Step 2
+    img, info = findFace(img)
+
+    ## Step 3
+    pError = trackFace(myDrone, info, w, pid, pError)
+
+    print(info[0][0])
+
+    cv2.imshow('Image', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        myDrone.land()
+        break
